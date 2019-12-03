5979c7991bb9dc02c9f34fe5486eb66d5a574c83
Andrey <a.ka144@mail.ru>
2019-12-03 18:08:19 +0300

    Добавил функцию подсчета количества задач, попадающих под фильтр

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,3 +1,4 @@
+
 const filterNames = [
   `all`,
   `overdue`,
@@ -8,11 +9,73 @@ const filterNames = [
   `archive`
 ];
 
-const generateFilters = () => {
+const generateFilters = (tasks) => {
+  const getCount = (filter) => {
+    let count = 0;
+
+    switch (filter) {
+      case `all`:
+        return tasks.length;
+
+      case `overdue`:
+        tasks.forEach((task) => {
+          if (task.dueDate instanceof Date &&
+            task.dueDate < Date.now()) {
+            count++;
+          }
+        });
+        break;
+
+      case `today`:
+        tasks.forEach((task) => {
+          if (task.dueDate instanceof Date &&
+            task.dueDate.getDate() === new Date().getDate()) {
+            count++;
+          }
+        });
+        break;
+
+      case `favorites`:
+        tasks.forEach((task) => {
+          if (task.isFavorite) {
+            count++;
+          }
+        });
+        break;
+
+      case `repeating`:
+        tasks.forEach((task) => {
+          if (Object.values(task.repeatingDays).some((it) => it)) {
+            count++;
+          }
+        });
+        break;
+
+      case `tags`:
+        tasks.forEach((task) => {
+          if (Array.from(task.tags).length !== 0) {
+            count++;
+          }
+        });
+        break;
+
+      case `archive`:
+        tasks.forEach((task) => {
+          if (task.isArchive) {
+            count++;
+          }
+        });
+        break;
+
+    }
+
+    return count;
+  };
+
   return filterNames.map((it) => {
     return {
       name: it,
-      count: Math.floor(Math.random() * 10),
+      count: getCount(it),
     };
   });
 };
