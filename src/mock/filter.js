deac5c26f521e4ffa6089770ddb2ea35e03704f7
Andrey <a.ka144@mail.ru>
2019-12-05 01:32:19 +0300

    Переделал функцию для фильтров

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -10,72 +10,49 @@ const filterNames = [
 ];
 
 const generateFilters = (tasks) => {
-  const getCount = (filter) => {
-    let count = 0;
+  const getCounts = () => {
+    const result = {
+      all: tasks.length,
+      overdue: 0,
+      today: 0,
+      favorites: 0,
+      repeating: 0,
+      tags: 0,
+      archive: 0
+    };
 
-    switch (filter) {
-      case `all`:
-        return tasks.length;
+    tasks.forEach((task) => {
+      if (task.dueDate instanceof Date &&
+        task.dueDate < Date.now()) {
+        result.overdue++;
+      }
+      if (task.dueDate instanceof Date &&
+        task.dueDate.getDate() === new Date().getDate()) {
+        result.today++;
+      }
+      if (task.isFavorite) {
+        result.favorites++;
+      }
+      if (Object.values(task.repeatingDays).some((it) => it)) {
+        result.repeating++;
+      }
+      if (Array.from(task.tags).length !== 0) {
+        result.tags++;
+      }
+      if (task.isArchive) {
+        result.archive++;
+      }
+    });
 
-      case `overdue`:
-        tasks.forEach((task) => {
-          if (task.dueDate instanceof Date &&
-            task.dueDate < Date.now()) {
-            count++;
-          }
-        });
-        break;
-
-      case `today`:
-        tasks.forEach((task) => {
-          if (task.dueDate instanceof Date &&
-            task.dueDate.getDate() === new Date().getDate()) {
-            count++;
-          }
-        });
-        break;
-
-      case `favorites`:
-        tasks.forEach((task) => {
-          if (task.isFavorite) {
-            count++;
-          }
-        });
-        break;
-
-      case `repeating`:
-        tasks.forEach((task) => {
-          if (Object.values(task.repeatingDays).some((it) => it)) {
-            count++;
-          }
-        });
-        break;
-
-      case `tags`:
-        tasks.forEach((task) => {
-          if (Array.from(task.tags).length !== 0) {
-            count++;
-          }
-        });
-        break;
-
-      case `archive`:
-        tasks.forEach((task) => {
-          if (task.isArchive) {
-            count++;
-          }
-        });
-        break;
-
-    }
-
-    return count;
+    return result;
   };
+
+  const counts = getCounts();
 
   return filterNames.map((it) => {
     return {
       name: it,
-      count: getCount(it),
+      count: counts[it],
     };
   });
 };
