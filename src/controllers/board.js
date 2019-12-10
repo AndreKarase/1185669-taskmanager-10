3a9f807e238ac35c3edc0f3308a925575e2e1a46
Andrey <a.ka144@mail.ru>
2019-12-11 02:47:16 +0300

    Объединил общие части в абстрактный класс, добавил контроллер

---
+++ src/controllers/board.js
@@ -0,0 +1,56 @@
+import LoadMoreButtonComponent from '../components/load-more-button.js';
+import TaskEditComponent from '../components/task-edit.js';
+import TaskComponent from '../components/task.js';
+import {render, remove, replace} from '../utils/render.js';
+
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
+
+export default class BoardController {
+  constructor(container) {
+    this._container = container;
+    this._loadMoreButtonComponent = new LoadMoreButtonComponent();
+  }
+
+  render(tasks) {
+    const container = this._container.getElement();
+
+    const renderTask = (task) => {
+      const taskComponent = new TaskComponent(task);
+      const taskEditComponent = new TaskEditComponent(task);
+
+      taskComponent.setEditButtonClickHandler(() => {
+        replace(taskEditComponent, taskComponent);
+      });
+
+      taskEditComponent.setSubmitHandler(() => {
+        replace(taskComponent, taskEditComponent);
+      });
+
+      render(taskListElement, taskComponent, `beforeend`);
+    };
+
+    const taskListElement = container.querySelector(`.board__tasks`);
+
+    let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+    for (let i = 0; i < showingTasksCount; i++) {
+      renderTask(tasks[i]);
+    }
+
+    render(container, this._loadMoreButtonComponent, `beforeend`);
+
+    this._loadMoreButtonComponent.setClickHandler(() => {
+      const prevTaskCount = showingTasksCount;
+      showingTasksCount += SHOWING_TASKS_COUNT_BY_BUTTON;
+
+      if (showingTasksCount >= tasks.length) {
+        showingTasksCount = tasks.length;
+        remove(this._loadMoreButtonComponent);
+      }
+
+      for (let i = prevTaskCount; i < showingTasksCount; i++) {
+        renderTask(tasks[i]);
+      }
+    });
+  }
+}
---
+++ src/utils/render.js
@@ -0,0 +1,34 @@
+export const render = (container, component, place) => {
+  switch (place) {
+    case `afterbegin`:
+      container.prepend(component.getElement());
+      break;
+    case `beforeend`:
+      container.append(component.getElement());
+      break;
+  }
+};
+
+export const remove = (component) => {
+  component.getElement().remove();
+  component.removeElement();
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const replace = (newComponent, oldComponent) => {
+  const newElement = newComponent.getElement();
+  const oldElement = oldComponent.getElement();
+  const parentElement = oldElement.parentElement;
+
+  const isExistElement = !!(newElement && oldElement && parentElement);
+
+  if (isExistElement) {
+    parentElement.replaceChild(newElement, oldElement);
+  }
+};
